7ede00af6d057662ce84aa03156135cbba86f373
wasif903 <wasifmehmood903@gmail.com>
2023-11-01 04:03:24 +0500

    push

--- routes/Summary.js
+++ routes/Summary.js
@@ -1,5 +1,5 @@
 import { YoutubeTranscript } from "youtube-transcript";
-import express from "express";
+import express, { response } from "express";
 import OpenAI from "openai";
 import axios from "axios";
 // // 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
@@ -11,19 +11,14 @@ import { GoogleAuth } from "google-auth-library";
 
 const router = express.Router();
 
-
-const hf = new HfInference("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");
+const openai = new OpenAI({
+  apiKey: "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",
+});
 
 router.post("/summary", async (req, res) => {
   try {
-    const MODEL_NAME = process.env.MODEL_NAME;
-    const API_KEY = process.env.API_KEY;
+    const { contentType, wordCounter, keyPoints } = req.body;
 
-    const client = new TextServiceClient({
-      authClient: new GoogleAuth().fromAPIKey(API_KEY),
-    });
-
-    // Check if the contentType is "paragraph" and wordCounter is provided
     if (
       req.body.contentType === "paragraph" &&
       req.body.wordCounter &&
@@ -32,178 +27,60 @@ router.post("/summary", async (req, res) => {
       const transcript = await YoutubeTranscript.fetchTranscript(
         req.body.vidURL
       );
-      const joined = transcript.map((item) => item.text).join(" ");
+      const joined = transcript
+        .map((item) => item.text)
+        .join(" ")
+        .slice(0, 500);
 
-      const promptString = joined;
-
-      const stopSequences = [];
-
-      const response = await client.generateText({
-        // required, which model to use to generate the result
-        model: MODEL_NAME,
-        // optional, 0.0 always uses the highest-probability result
-        temperature: 0.6,
-        // optional, how many candidate results to generate
-        candidateCount: 1,
-        // optional, number of most probable tokens to consider for generation
-        top_k: 40,
-        // optional, for nucleus sampling decoding strategy
-        top_p: 0.95,
-        // optional, maximum number of output tokens to generate
-        max_output_tokens: 1024,
-        // optional, sequences at which to stop model generation
-        stop_sequences: req.body.wordCounter,
-
-        // optional, safety settings
-        safety_settings: [
-          { category: "HARM_CATEGORY_DEROGATORY", threshold: 1 },
-          { category: "HARM_CATEGORY_TOXICITY", threshold: 1 },
-          { category: "HARM_CATEGORY_VIOLENCE", threshold: 2 },
-          { category: "HARM_CATEGORY_SEXUAL", threshold: 2 },
-          { category: "HARM_CATEGORY_MEDICAL", threshold: 2 },
-          { category: "HARM_CATEGORY_DANGEROUS", threshold: 2 },
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `Summarize This Content In ${wordCounter}, here is the content ${joined}`,
+          },
         ],
-        prompt: {
-          text: ` ${promptString} Give Me The Summary Of This Content In Maximum ${req.body.wordCounter} Amount of words not in keypoints`,
-        },
+        temperature: 0,
+        max_tokens: 1024,
       });
 
-      const outputArray = await response.map((item) => {
-        // Check if 'item' is not null and has the expected structure
-        if (
-          item &&
-          item.candidates &&
-          item.candidates[0] &&
-          item.candidates[0].output
-        ) {
-          return item.candidates[0].output;
-        }
-        // Return null for invalid or missing data
-        return null;
-      });
-
-      // Filter out null values
-      const filteredOutputArray = outputArray.filter((item) => item !== null);
-      // Join the elements of filteredOutputArray into a single string with newline separators
-      const responseString = filteredOutputArray.join("\n");
-
-      // Counter Code
-      const findCouters = await counter.find();
-      const reversed = findCouters.reverse();
-      const currentCount = reversed[0];
-      if (findCouters.length !== 0) {
-        const createCount = new counter({
-          counter: currentCount.counter + 1,
-        });
-        await createCount.save();
-      } else {
-        const createCount = new counter({
-          counter: 1,
-        });
-        await createCount.save();
-      }
-
-      // Now 'responseString' contains the entire response as a single string
-
-      res.status(200).json(responseString);
+      const data = response.choices[0].message.content;
+      res.status(200).json(data);
     } else if (
       req.body.contentType === "points" &&
-      req.body.keyPoints &&
-      !req.body.wordCounter
+      !req.body.wordCounter &&
+      req.body.keyPoints
     ) {
       const transcript = await YoutubeTranscript.fetchTranscript(
         req.body.vidURL
       );
-      const joined = transcript.map((item) => item.text).join(" ");
+      const joined = transcript
+        .map((item) => item.text)
+        .join(" ")
+        .slice(0, 500);
 
-      const promptString = joined;
-
-      const stopSequences = [];
-
-      const response = await client.generateText({
-        // required, which model to use to generate the result
-        model: MODEL_NAME,
-        // optional, 0.0 always uses the highest-probability resultadadada
-        temperature: 0.6,
-        // optional, how many candidate results to generate
-        candidateCount: 1,
-        // optional, number of most probable tokens to consider for generation
-        top_k: 40,
-        // optional, for nucleus sampling decoding strategy
-        top_p: 0.95,
-        // optional, maximum number of output tokens to generate
-        max_output_tokens: req.body.keyPoints,
-        // optional, sequences at which to stop model generation
-        stop_sequences: stopSequences,
-
-        // optional, safety settings
-        safety_settings: [
-          { category: "HARM_CATEGORY_DEROGATORY", threshold: 1 },
-          { category: "HARM_CATEGORY_TOXICITY", threshold: 1 },
-          { category: "HARM_CATEGORY_VIOLENCE", threshold: 2 },
-          { category: "HARM_CATEGORY_SEXUAL", threshold: 2 },
-          { category: "HARM_CATEGORY_MEDICAL", threshold: 2 },
-          { category: "HARM_CATEGORY_DANGEROUS", threshold: 2 },
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `Summarize This Content Strictly In ${keyPoints} bullet points with numbering, here is the content ${joined}`,
+          },
         ],
-        prompt: {
-          text: `${promptString} Give Me The Summary Of This Content In Maximum ${req.body.keyPoints} Key Points With No Heading and with not in paragraph`,
-        },
+        temperature: 0,
+        max_tokens: 1024,
       });
 
-      const outputArray = await response.map((item) => {
-        // Check if 'item' is not null and has the expected structure
-        if (
-          item &&
-          item.candidates &&
-          item.candidates[0] &&
-          item.candidates[0].output
-        ) {
-          return item.candidates[0].output;
-        }
-        // Return null for invalid or missing data
-        return null;
-      });
+      const data = response.choices[0].message.content;
 
-      // Filter out null values
-      const filteredOutputArray = outputArray.filter((item) => item !== null);
-
-      const responseString = filteredOutputArray.join("\n");
-
-      // Split the input string into individual points
-      const pointsArray = responseString
-        .split(/\n\d+\.\s+|\s+-\s+/)
-        .filter(Boolean);
-
-      // Create an array of objects with index and point
-      const pointsObjects = pointsArray.map((point, index) => ({
-        index: index + 1, // Add 1 to make the index 1-based
-        point: point.replace(/^\d+\.\s+/, "").trim(), // Remove leading index and whitespace
+      const keyPointsArray = data.split("\n").map((point, index) => ({
+        content: point,
       }));
-      // 'pointsObjects' now contains an array of objects with index and point
-      // console.log(pointsObjects);
 
-      // Counter Code
-      const findCouters = await counter.find();
-      const reversed = findCouters.reverse();
-      const currentCount = reversed[0];
-      if (findCouters.length !== 0) {
-        const createCount = new counter({
-          counter: currentCount.counter + 1,
-        });
-        await createCount.save();
-      } else {
-        const createCount = new counter({
-          counter: 1,
-        });
-        await createCount.save();
-      }
-
-      res.status(200).json(pointsObjects);
-    } else {
-      res.status(400).json({ message: "Invalid Request" });
+      res.status(200).json(keyPointsArray);
     }
   } catch (error) {
-    console.error(error);
+    console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
